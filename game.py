4b628c0e5606af1019a79773cee3acf08dcbcbe4
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-05 01:18:23 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -2,19 +2,19 @@ import random
 
 def game(comp,you):
     if(comp == you):
-        return Draw
-    elif(comp == s and you == w):
-        return lost
-    elif(comp == s and you == g):
-        return win
-    elif(comp == g and you == w):
-        return win
-    elif(comp == g and you == s):
-        return lost
-    elif(comp == w and you == s):
-        return win
-    elif(comp == w and you == g):
-        return lost
+        return 'Draw'
+    elif(comp == 's' and you == 'w'):
+        return 'lost'
+    elif(comp == 's' and you == 'g'):
+        return 'win'
+    elif(comp == 'g' and you == 'w'):
+        return 'win'
+    elif(comp == 'g' and you == 's'):
+        return 'lost'
+    elif(comp == 'w' and you == 's'):
+        return 'win'
+    elif(comp == 'w' and you == 'g'):
+        return 'lost'
 
 print("Computer Choice :> Snake(s)    Water(w)    Gun(g)")
 ranChoice = random.randint(1, 3)
@@ -24,8 +24,8 @@ elif (ranChoice == 2):
     comp = 'w'
 elif (ranChoice == 3):
     comp = 'g' 
-user = input("Your Choice :> Snake(s)    Water(w)    Gun(g)")
+you = input("Your Choice :> Snake(s)    Water(w)    Gun(g)")
 
-result = game(comp, user)
+result = game(comp, you)
 print("Computer Choice :>" +str(comp))
-print("User Choice :>" +str(user))
+print("User Choice :>" +str(you))
